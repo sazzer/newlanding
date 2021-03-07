ca6fe68ccbf85f6869eb9b665cd2cc49cf8f3a59
Graham Cox <graham@grahamcox.co.uk>
2021-03-07 12:51:01 +0000

    Set up Tracing, including Jaeger support

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,4 +1,25 @@
+#![deny(clippy::all, clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
+
+use dotenv::dotenv;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+
 #[actix_rt::main]
 async fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    env_logger::init();
+
+    opentelemetry::global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let (tracer, _uninstall) = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("bigbang")
+        .from_env()
+        .install()
+        .unwrap();
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default().with(telemetry);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    tracing::info!("Hello");
 }
