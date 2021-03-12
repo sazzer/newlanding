c59a14e33b27c2d343f152733df2e23e0f7dcff8
Graham Cox <graham@grahamcox.co.uk>
2021-03-12 20:57:14 +0000

    Set up Echo web server

--- service/internal/server/server.go
+++ service/internal/server/server.go
@@ -3,17 +3,35 @@ package server
 import (
 	"fmt"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 )
 
 // Wrapper around the HTTP server.
 type Server struct {
-	port uint16
+	port   uint16
+	server *echo.Echo
 }
 
 // Create a new instance of the HTTP server.
 func New(port uint16) Server {
-	return Server{port}
+	e := echo.New()
+
+	e.Use(middleware.RequestID())
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.Decompress())
+	e.Use(middleware.Gzip())
+
+	return Server{
+		port:   port,
+		server: e,
+	}
 }
 
 // Start the HTTP Server listening.
@@ -21,4 +39,8 @@ func (s Server) Start() {
 	address := fmt.Sprintf(":%d", s.port)
 
 	log.Info().Str("address", address).Msg("Starting HTTP Server")
+
+	if err := s.server.Start(address); err != nil {
+		log.Fatal().Err(err).Msg("Failed to start HTTP server")
+	}
 }
