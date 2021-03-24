00415f6409077ed7dfd87b2b0dc855acf3ca2f3f
Graham Cox <graham@grahamcox.co.uk>
2021-03-24 07:51:58 +0000

    Logging

--- service/cmd/newlanding/main.go
+++ service/cmd/newlanding/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).With().Caller().Logger()
+
+	log.Info().Msg("Hello")
 }
--- service/go.mod
+++ service/go.mod
@@ -1,3 +1,8 @@
 module github.com/sazzer/newlanding/service
 
 go 1.16
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.20.0
+)
